4f63b7b4bffb7c8789ca3bbfcaadcc90baf1edf0
neskines <nmotieno@myseneca.ca>
2023-01-23 01:20:06 -0500

    init

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
 
 app.get("/", (req, res)=> {res.json('API Listening')});
 
