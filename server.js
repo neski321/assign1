ac121f8d14e7ee606f0f2d15d1ba71943c943a0d
neskines <nmotieno@myseneca.ca>
2023-01-23 00:46:56 -0500

    init

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res)=> {res.json('API Listening')});
+
+const MoviesDB = require("./modules/moviesDB.js");
+const db = new MoviesDB();
+
+db.initialize(process.env.MONGODB_CONN_STRING).then(()=>{
+    app.listen(HTTP_PORT, ()=>{
+    console.log(`server listening on: ${HTTP_PORT}`);
+    });
+   }).catch((err)=>{
+    console.log(err);
+   });
+
+app.post("/api/movies", (req,res) => {
+    db.addNewMovie(req.body)
+    .then(() => {
+            res.status(201).json(`new movie successfully added`);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
+
+app.get("/api/movies", (req,res) => {
+    db.getAllMovies(req.query.page, req.query.perPage)
+        .then((movies) => {
+            res.status(200).json(movies);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
+
+app.get("/api/movies/:id", (req,res) => {
+    db.getMovieById(req.params.id)
+        .then((sales) => {
+            res.status(200).json(sales);
+        })
+        .catch((err) => {
+            res.status(404).json(err);
+        });
+});
+
+app.put("/api/movies/:id", (req,res) => {
+    db.updateMovieById(req.body, req.params.id)
+        .then(() => {
+            res.status(200).json(`Movie ${req.body._id} successfully updated`);
+        })
+        .catch((err) => {
+            res.status(404).json(err);
+        });
+});
+
+app.delete("/api/movies/:id", (req,res) => {
+    db.deleteMovieById(req.params.id)
+        .then(() => {
+            res.status(200).json(`Movie ${req.params.id} successfully deleted`);
+        })
+        .catch((err) => {
+            res.status(404).json(err);
+        });
+});
