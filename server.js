0ae42401474c20cd708a51fe1949ecb83071e50f
neskines <nmotieno@myseneca.ca>
2023-01-23 09:31:28 -0500

    init

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+
+/*********************************************************************************
+* BTI425 – Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Neskines Otieno Student ID: 118317205 Date:2023-01-22
+* Cyclic Link: https://sleepy-erin-coral.cyclic.app/
+*
+********************************************************************************/
+
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
